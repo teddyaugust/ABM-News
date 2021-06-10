df063c4b3adc05ab34e828205f0c6ad40a60a5c0
teddyaugust <loyalcoachsystem@gmail.com>
2021-06-10 14:39:53 +0700

    Version Code 3 Version Name 1.3

---
+++ app/src/main/java/com/teddy/abmnews/KontakActivity.java
@@ -0,0 +1,179 @@
+package com.teddy.abmnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class KontakActivity extends AppCompatActivity {
+
+    //Inisialiasi Variabel
+    DrawerLayout drawerLayout;
+    WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontak);
+
+        //buka webnya
+        webView = findViewById(R.id.kontak_webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://abm.yuk-simak.info/wp/contact");
+
+        WebSettings webSettings = webView.getSettings();
+
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDisplayZoomControls(false);
+        webSettings.supportZoom();
+        webSettings.setSupportZoom(true);
+        webSettings.setBuiltInZoomControls(true);
+
+        webSettings.setLoadWithOverviewMode(true);
+        webSettings.setUseWideViewPort(true);
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView wv, String url) {
+                if(url.startsWith("tel:") || url.startsWith("whatsapp:") || url.startsWith("tg:") || url.startsWith("facebook:") ) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        //Assign variabel
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+    }
+    public void ClickMenu(View view){
+        //Open drawer
+        openDrawer (drawerLayout);
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //Open Drawer Layout
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void ClickLogo(View view){
+        //Close Drawernya
+        closeDrawer(drawerLayout);
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //Close Drawer Layout
+        //Check Condition
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            //Jika drawernya masih terbuka
+            //maka tutup Drawer-nya
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    public void ClickHome(View view){
+        //Redirect activity ke Beranda
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void ClickLogin (View view){
+        //Redirect activity ke Login
+        MainActivity.redirectActivity(this,LoginActivity.class);
+    }
+
+    public void ClickCompany (View view){
+        //Redirect activity ke Company Profile
+        MainActivity.redirectActivity(this,ProfilActivity.class);
+    }
+
+    public void ClickProduk (View view){
+        //Redirect activity ke Produk
+        MainActivity.redirectActivity(this,ProdukActivity.class);
+    }
+
+    public void ClickPlan (View view){
+        //Redirect activity ke Marketing Plan
+        MainActivity.redirectActivity(this,PlanActivity.class);
+    }
+
+    public void ClickTestimoni (View view){
+        //Redirect activity ke Testiminoni
+        MainActivity.redirectActivity(this,TestimoniActivity.class);
+    }
+
+    public void ClickInfoTerkini (View view){
+        //Redirect activity ke Info Terkini
+        MainActivity.redirectActivity(this,InfoActivity.class);
+    }
+
+    public void ClickTips (View view){
+        //Redirect activity ke Tips
+        MainActivity.redirectActivity(this,TipsActivity.class);
+    }
+
+    public void ClickLeaders (View view){
+        //Redirect activity ke Leaders
+        MainActivity.redirectActivity(this,LeadersActivity.class);
+    }
+
+    public void ClickTutorial (View view){
+        //Redirect activity ke Tutorial
+        MainActivity.redirectActivity(this,TutorialActivity.class);
+    }
+
+    public void ClickKontak (View view){
+        //Recreate activity
+        recreate();
+    }
+
+    public void ClickTentang (View view){
+        //Redirect activity ke Tentang
+        MainActivity.redirectActivity(this,TentangActivity.class);
+    }
+
+    public void ClickLogout (View view){
+        //Menutup Aplikasi
+        MainActivity.logout(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //Close Drawer
+        closeDrawer(drawerLayout);
+    }
+
+}
+
